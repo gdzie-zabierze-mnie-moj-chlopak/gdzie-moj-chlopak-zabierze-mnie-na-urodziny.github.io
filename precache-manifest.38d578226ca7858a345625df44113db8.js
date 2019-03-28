b0ca27fa279ac800358f78bb301ef089a4e12dbb
Bartosz Łągwa <bartosz.lagwa@neurosoft.pl>
2019-03-28 08:24:18 +0100

    Updates

---
+++ precache-manifest.38d578226ca7858a345625df44113db8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "395283fe5fece13b8c21",
+    "url": "/static/js/main.204caf0f.chunk.js"
+  },
+  {
+    "revision": "ed336867ff9a50005a20",
+    "url": "/static/js/2.1577f0aa.chunk.js"
+  },
+  {
+    "revision": "395283fe5fece13b8c21",
+    "url": "/static/css/main.ab32c4b6.chunk.css"
+  },
+  {
+    "revision": "ed336867ff9a50005a20",
+    "url": "/static/css/2.9e6757e3.chunk.css"
+  },
+  {
+    "revision": "b2dbaec5e17cab18a081beaac480aad3",
+    "url": "/index.html"
+  }
+];
